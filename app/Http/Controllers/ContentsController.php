e534d7de33ac08096538d2ffde17232d490d67a1
ayato-ininin <sarai.a.1203408@gmail.com>
2021-01-01 07:45:07 +0900

    add

--- app/Http/Controllers/ContentsController.php
+++ app/Http/Controllers/ContentsController.php
@@ -13,14 +13,14 @@ public function post(Request $request){
     $param=[
         "habit_id"=>$request->habit_id,
         "user_id"=>$request->user_id,
-        "content"=>$request->content,
+        "contain"=>$request->contain,
         "point"=>$request->point,
         "created_at"=>$now,
         "updated_at"=>$now
     ];
     DB::table('contents')->insert($param);
     return response()->json([
-        'message'=>'contens created suceessfully',
+        'message'=>'contents created suceessfully',
         'data'=>$param
     ],200);
 }
