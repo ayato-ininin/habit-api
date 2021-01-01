1cd7bb1b00ec53ae74dae9fc1ca5f1dcad0eeafc
ayato-ininin <sarai.a.1203408@gmail.com>
2021-01-01 10:04:00 +0900

    add

---
+++ database/migrations/2021_01_01_010016_rename_contents_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class RenameContentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('contents', function (Blueprint $table) {
+            $table->renameColumn('habit-id','habit_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('contents', function (Blueprint $table) {
+            $table->renameColumn('habit_id','habit-id');
+        });
+    }
+}
